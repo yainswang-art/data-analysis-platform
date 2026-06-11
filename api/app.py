0231a6a2ea15c221332a82b05d5dc13b1377352a
yainswang-art <yainswang@gmail.com>
2026-06-12 02:55:35 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -4,10 +4,11 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "API running 🚀"}
+    return {"message": "API is working 🚀"}
 
 @app.get("/data")
 def data():
     return {
-        "sales": [100, 200, 150, 300]
+        "sales": [100, 200, 150, 300],
+        "profit": [20, 50, 30, 80]
     }
